148947ae15e8c1ff13c91a9df61411c434007139
Sami Rollins <srollins@gmail.com>
2023-09-05 15:23:31 -0400

    upload examples

---
+++ 9.6.2023/seconds_to_minutes.py
@@ -0,0 +1,14 @@
+'''
+A program to prompt a user to enter a number of seconds and 
+covert that to minutes.
+
+# https://docs.python.org/3/tutorial/inputoutput.html
+
+'''
+
+def main():
+
+
+
+if __name__ == '__main__':
+	main()	
