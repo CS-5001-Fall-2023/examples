774ddcb6fe9d0ccd354ecce7a189d67440e89675
Sami Rollins <srollins@gmail.com>
2023-09-06 19:43:39 -0400

    upload seconds to minutes example

--- 9.6.2023/seconds_to_minutes.py
+++ 9.6.2023/seconds_to_minutes.py
@@ -1,13 +1,26 @@
-'''
+"""
 A program to prompt a user to enter a number of seconds and 
 covert that to minutes.
 
 # https://docs.python.org/3/tutorial/inputoutput.html
 
-'''
+"""
 
 def main():
+	# ask the user for the number
+	#    - store the number
+	seconds = int(input('Enter a number of seconds: '))
+	
+	# print('You entered',seconds,'seconds.')
+	print(f'You entered {seconds} seconds.')
+	# print(type(seconds))
 
+	# math operators: + - * / % //
+	# perform the calculation from s to m
+	minutes = seconds / 60
+
+	# output the result
+	print(f'{seconds} seconds is {minutes} minutes')
 
 
 if __name__ == '__main__':
