07674227e6122c5f13846060376d1ff65b0eb1df
Sami Rollins <srollins@gmail.com>
2023-09-07 13:41:43 -0400

    update examples

---
+++ 9.11.2023/cigar_party.py
@@ -0,0 +1,19 @@
+'''
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise.
+'''
+
+def cigar_party(cigars: int, is_weekend: bool) -> bool:
+	pass
+
+def main():
+	
+	result = cigar_party(70, True)
+	print(f'cigar_party(70, True) is {result}')
+
+ 	# How many tests do we need to do
+	# to ensure that our function
+	# is correct?
+
+
+if __name__ == '__main__':
+	main()
---
+++ 9.11.2023/conditions.py
@@ -0,0 +1,56 @@
+'''
+A sample program to demonstrate the syntax of conditional statements.
+'''
+
+def main():
+	# Exercise 1
+	# if the value stored in the variable 
+	# number is less than 10 print 
+	# 'Number is small'
+	# otherwise print
+	# 'Number is not small'
+
+	# Exercise 2
+	# if the value stored in the variable number
+	# is greater than 15 print
+	# 'Seems like a big number'
+	# 'Are you sure that's right?'
+	# otherwise print
+	# 'Good job!'
+	# 'You picked a good number.'
+
+
+	# Exercise 3
+	# if the string stored in the variable animal
+	# is 'dog' print 'woof'
+	# is 'cow' print 'moo'
+	# is 'bird' print 'chirp'
+
+	# Exercise 4
+	# if the value stored in the variable first_name
+	# is 'Sami' and the value stored in the variable
+	# last_name is 'Rollins' print
+	# 'Hey, that's my name!'
+	# otherwise
+	# if the value stored in the variable first_name
+	# is 'Sami' print 
+	# 'Cool first name...haha'
+	# otherwise if the value stored in the variable last_name is 'Rollins' print
+	# 'Cool last name'
+
+	# Exercise 5
+	# if the value stored in the variable user_choice is not a valid rock, paper, scissors choice print
+	# 'Invalid choice'
+	# otherwise print
+	# 'Valid choice'
+	# use only and and not or in your solution
+
+	# Exercise 6
+	# if the value stored in the variable user_choice is not a valid rock, paper, scissors choice print
+	# 'Invalid choice'
+	# otherwise print
+	# 'Valid choice'
+	# use only or and not and in your solution
+
+if __name__ == '__main__':
+	main()
