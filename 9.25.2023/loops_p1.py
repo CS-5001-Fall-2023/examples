1813d3c6d03d6582a8bd003cc5d6749365fa3885
Sami Rollins <srollins@gmail.com>
2023-09-19 17:03:25 -0400

    update examples

---
+++ 9.25.2023/loops_p1.py
@@ -0,0 +1,46 @@
+'''
+Practice with iteration and while loops!
+'''
+
+def print_to_n(n: int):
+	'''
+	Print all numbers from 1 to n inclusive.
+	'''
+	pass
+
+def print_evens(n: int):
+	'''
+	Print all even numbers from 1 to n inclusive.
+	You should be able to implement this using 
+	two different approaches!
+	'''
+	pass
+
+def guess_my_word(secret_word: str):
+	'''
+	Prompt the user to enter a word until the user's
+	word matches the secret word.
+	Modification: count the number of guesses.
+	Modification: give the user a maximum number of
+	tries.
+	'''
+	pass
+
+def guess_my_number(secret_number: int):
+	'''
+	Prompt the user to enter an integer until the user's
+	number matches the secret number.
+	For incorrect guesses, tell the user whether the
+	guess was too high or too low.
+	'''
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
+
+
+
---
+++ 9.25.2023/strings_lists.py
@@ -0,0 +1,22 @@
+'''
+Practice with strings and lists!
+'''
+
+def guess_my_word(secret_word: str):
+	'''
+	Prompt the user to enter a word until the user's
+	word matches the secret word.
+	Keep a list of all of the previous guesses the 
+	user has made.
+	'''
+	pass
+
+# additional examples pending
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
