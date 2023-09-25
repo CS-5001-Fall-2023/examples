3e059f98146af601cc078e0324647d24ee7d8399
Sami Rollins <srollins@gmail.com>
2023-09-25 18:48:06 -0400

    partial solution for guess_my_word

--- 9.25.2023/loops_p1.py
+++ 9.25.2023/loops_p1.py
@@ -6,7 +6,12 @@ def print_to_n(n: int):
 	'''
 	Print all numbers from 1 to n inclusive.
 	'''
-	pass
+	current_number = 1 # initialization of control variable
+	
+	while current_number <= n:
+		print(current_number)
+		current_number += 1 # update control variable
+
 
 def print_evens(n: int):
 	'''
@@ -14,7 +19,19 @@ def print_evens(n: int):
 	You should be able to implement this using 
 	two different approaches!
 	'''
-	pass
+	current_number = 2 # initialization of control variable
+	
+	while current_number <= n:
+		print(current_number)
+		current_number += 2 # update control variable
+
+
+	# current_number = 1 # initialization of control variable
+	
+	# while current_number <= n:
+	# 	if current_number % 2 == 0:
+	# 		print(current_number)
+	# 	current_number += 1 # update control variable
 
 def guess_my_word(secret_word: str):
 	'''
@@ -24,7 +41,16 @@ def guess_my_word(secret_word: str):
 	Modification: give the user a maximum number of
 	tries.
 	'''
-	pass
+	MAX_GUESSES = 5
+	user_guess = input('Guess a word: ')
+	guesses = 1
+
+	while user_guess != secret_word:		
+		user_guess = input('WRONG! Guess again: ')
+		guesses += 1
+
+	print(f'You guessed the secret word: {secret_word} in {guesses} guess(es).')
+
 
 def guess_my_number(secret_number: int):
 	'''
@@ -36,7 +62,9 @@ def guess_my_number(secret_number: int):
 	pass
 
 def main():
-	pass
+	# print_to_n(5)
+	# print_evens(5)
+	guess_my_word('secret')
 
 if __name__ == '__main__':
 	main()
