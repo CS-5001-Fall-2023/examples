7b41b48ed43ab5eacf439a40de17d8d5721b13ee
Sami Rollins <srollins@gmail.com>
2023-09-27 19:40:34 -0400

    update strings and lists examples

--- 9.25.2023/lists.py
+++ 9.25.2023/lists.py
@@ -13,16 +13,36 @@ def fun_with_lists():
 	# Appending to a list
 	# Lists of mixed types
 	# Slicing a list
-	pass
+	names = ['amy', 'anna', 'bob']
+	values = [34, 5, 2, 5, 17]
+	mixed_type_list = ['may', 15, 73.5]
+	mixed_type_list.append('hello')
+	print(values[1:3])
+
+	# index = 0
+	# while index < len(mixed_type_list):
+	# 	print(mixed_type_list[index])
+	# 	index += 1
+	# mixed_type_list[0] = 'june'
+	# index = 0
+	# while index < len(mixed_type_list):
+	# 	print(mixed_type_list[index])
+	# 	index += 1
 
 
 def list_to_string(list_of_string: list[str]) -> str:
 	'''
 	A function that takes a list of strings and
 	combines them into a single string with 
-	spaces between each word. Returns the string.
+	spaces between each word. Returns the string.		
 	'''
-	pass
+	result = ''
+	index = 0
+	while index < len(list_of_string):
+		result += list_of_string[index] + ' '
+		index += 1
+	return result
+
 
 def greater_than_100(values: list[int]) -> int:
 	'''
@@ -30,10 +50,23 @@ def greater_than_100(values: list[int]) -> int:
 	and returns the number of ints in the list that
 	are greater than 100.
 	'''
-	pass
+	TARGET = 100
+	count = 0
+	index = 0
+	while index < len(values):
+		if values[index] > TARGET:
+			count += 1
+		index += 1
+	return count
 
 def main():
-	pass
+	# fun_with_lists()
+	# values = []
+	# result = greater_than_100(values)
+	# print(result)
+	months = ['may', 'june', 'august', 'september']
+	result = list_to_string(months)
+	print(result)
 
 if __name__ == '__main__':
 	main()
--- 9.25.2023/strings.py
+++ 9.25.2023/strings.py
@@ -11,14 +11,32 @@ def fun_with_strings():
 	# Accessing individual characters
 	# Calling string methods -- like replace!
 	# Slicing strings
-	pass
+	name = 'karlo berket'
+	# first, last = name.split()
+	# print(first)
+	# print(last)
+	# # user_name = first[-1] + '.' + last
+	# user_name = first[len(first)-1] + '.' + last
+	# print(user_name)
+
+	sliced = name[0:9:2]
+	print(sliced)
+
+	# print(name[5])
+	# if name.endswith('ket'):
+	# 	print(name)
+	# 	print('your string ends with ket')
+	# else:
+	# 	print('bad ending')
+	# result = name.find('ket')
+	# print(result)
 
 
 def reverse(phrase: str) -> str:
 	'''
 	A function to reverse the string passed as input.
 	'''
-	pass
+	return phrase[::-1]
 
 def is_palindrome(word: str) -> bool:
 	'''
@@ -30,11 +48,33 @@ def is_palindrome(word: str) -> bool:
 	Consider two different approaches to implementing 
 	this function.
 	'''
-	pass	
+	return word == reverse(word)
+
+	# if word == reverse(word):
+	# 	return True
+	# else:
+	# 	return False
+
+	# first_index = 0
+	# last_index = len(word) - 1 
+	# while first_index < last_index:
+	# 	if word[first_index] == word[last_index]:
+	# 		first_index += 1
+	# 		last_index -= 1
+	# 	else:
+	# 		return False
+	# return True
+
+
+
 
 def main():
-	pass
+	# fun_with_strings()
+	result = is_palindrome('died')
+	print(f'result: {result}')
 
+	# result = reverse('class')
+	# print(f'result: {result}')
 
 
 if __name__ == '__main__':
