9403468b19f350e39253c39d24400269d1e98a8c
Sami Rollins <srollins@gmail.com>
2023-10-02 19:21:12 -0400

    update examples

---
+++ 10.2.2023/loops_p2.py
@@ -0,0 +1,81 @@
+'''
+Practice with for loops!
+'''
+
+def fun_with_for_loops():
+	# Remember to call this function :)
+	list = ["hello", "there", "cs", "5001", "class"]
+	# i = 0
+	# while i < len(list):
+	# 	item = list[i]		
+	# 	print(f'{i} {item}')
+	# 	i += 1
+
+	# 15
+	# 0 5 10 15
+
+	# Iterating over a list is easier with a for loop!
+	# string = 'hello class'
+	# for item in list:
+	# 	print(item)
+
+	# Accessing the index using range()
+	# for i in range(1, len(list), 2):
+	# 	item = list[i]
+	# 	print(f'{i} {item}')
+
+	# Accessing the index using enumerate
+	# for value in enumerate(list):
+	# 	print(value)	
+	# for i, item in enumerate(list):
+	# 	print(f'{i} --- {item}')	
+
+	# Using zip to iterate over two lists at once
+	first_names = ['joe', 'jane', 'julio']
+	last_names = ['berket', 'saberi', 'vega']
+
+	for first, last in zip(first_names, last_names):
+		print(f'First: {first} Last: {last}')
+
+
+def multiples_of_5(number: int):
+	'''
+	A function that takes as input a positive integer and
+	uses a for loop to print the multiples of 5 that are 
+	less than or equal to the number that was entered.
+	'''
+	for i in range(0, number+1, 5):
+		# if i % 5 == 0:
+		# 	print(i)
+		print(i)
+
+def count_vowels(sentence: str) -> int:
+	'''
+	A function that takes as input a string and returns the number
+	of vowels (a, e, i, o, u) that are found in the sentence.
+	'''
+	pass
+
+def generate_user_names(first_names: list[str], 
+	last_names: list[str]) -> list[str]:
+	'''
+	A function that takes as input a list of strings
+	representing first names and a list of strings representing
+	last names and returns a list of usernames where the ith username
+	is the first initial of the first name stored at the ith position
+	of the first_names list concatenated with the first 7 characters 
+	of the last name stored at the ith position of the last_names
+	list.
+	'''
+	pass
+
+def nested_loops():
+	pass
+
+def main():
+	# fun_with_for_loops()
+	multiples_of_5(15)
+
+if __name__ == '__main__':
+	main()
+
