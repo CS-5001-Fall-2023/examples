877c41f464a009e2198029f6720614ba7d87563e
Sami Rollins <srollins@gmail.com>
2023-10-11 19:44:21 -0400

    update recursion examples

--- 10.11.2023/recursion.py
+++ 10.11.2023/recursion.py
@@ -7,8 +7,11 @@ def factorial_iterative(n: int) -> int:
 	A function that takes as input an integer and returns the 
 	factorial of that number.
 	"""
-	pass
-
+	result = 1
+	for i in range(1, n+1):
+		# result = result * i  
+		result *= i  
+	return result
 
 def factorial_recursive(n: int) -> int:
 	"""
@@ -16,28 +19,44 @@ def factorial_recursive(n: int) -> int:
 	factorial of that number.
 	This implementation does not use a loop.
 	"""
-	pass
+	if n == 1:
+		return 1	
+	return n * factorial_recursive(n-1)
 
 def print_string(string: str):
 	"""
 	Implement a recursive function that takes as input a str and prints 
 	the characters of the str one per line *without using a loop*. 
 	"""
-	pass
+	# if len(string) == 1:
+	# 	print(string)	
+	# else:
+	# 	print(string[0])
+	# 	print_string(string[1:])	
+
+	if len(string) == 0:
+		return
+	print(string[0])
+	print_string(string[1:])	
 
 def print_string_backward(string: str):
 	"""
 	Implement a recursive function that takes as input a str and prints 
 	the characters of the str *in reverse* one per line *without using a loop*. 
 	"""
-	pass
+	if len(string) == 0:
+		return
+	print_string_backward(string[1:])	
+	print(string[0])
+	
 
 def print_nums_iterative(n: int):
 	"""
 	A function that takes as input a number and prints from 1 to 
 	the number (inclusive).
 	"""
-	pass
+	for i in range(1, n+1):
+		print(i)
 
 def print_nums_recursive(n: int):
 	"""
@@ -45,8 +64,22 @@ def print_nums_recursive(n: int):
 	There are several ways to do this. We will look at how to
 	use a helper function.
 	"""
-	pass
+	# if n <= 0:
+	# 	return
+	# if n == 1:
+	# 	print(n)
+	# 	return
+	# print(n)
+	# print_nums_recursive(n-1)
+	print_nums_helper(1, n)
 
+def print_nums_helper(current: int, n: int):
+	# print_nums_helper(1, n)
+	if current == n:
+		print(n)
+		return
+	print(current)
+	print_nums_helper(current+1, n)	
 
 def find_char_a(string: str) -> int:
 	"""
@@ -75,7 +108,7 @@ def binarysearch(list: list[int], target: int) -> bool:
  	pass
 
 def main():
-	pass
+	print_nums_recursive(5)
 
 if __name__ == '__main__':
 	main()
